fa2eff93043c9602c4a5077ebfe48a82c2698912
Frank Wolf <fwolf@posteo.de>
2021-12-31 10:59:58 +0100

    feat: dockerize golang app

--- exercises/go-helloworld/main.go
+++ exercises/go-helloworld/main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Hello World")
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World")
+}
+
+type Response struct {
+	Status string
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	response := Response{Status: "OK - endpoint is up"}
+
+	responseJson, err := json.Marshal(response)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseJson)
 }
 
 func main() {
-    http.HandleFunc("/", helloWorld)
-    http.ListenAndServe(":6111", nil)
+	http.HandleFunc("/status", health)
+	http.HandleFunc("/", helloWorld)
+	http.ListenAndServe(":6111", nil)
 }
